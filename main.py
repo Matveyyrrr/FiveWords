61688b8714f907018f140a1d4a88ad17fc8d4bb9
ildarishe <53740638+ildarishe@users.noreply.github.com>
2023-01-14 10:16:56 +0300

    Add files via upload
    Версия 1.0. Все работает, но нужны доработки. GameOver-Win-Start окна. И небольшие недочеты в логике игры

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 import random
-
 import pygame
 import sys
 
 pygame.init()
 
-SIZE = (700, 450)
+SIZE = (650, 500)
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("5 букв")
 WIDTH = 40
@@ -20,22 +19,13 @@ LETTER_COLORS = {
     3: ()
 }
 
-#NORMAL_WORDS = None
+# NOUNS = load_from_file("words.txt")
+# NORMAL_WORDS = load_from_file("")
 
+STAGES = {1: 'stage_1.png', 2: 'stage_3.png',
+          3: 'stage_5.png', 4: 'stage_6.png',
+          5: 'stage_8.png', 6: 'stage_10.png', }
 
-def load_from_file(file_name):
-    f = open(file_name, "r", encoding="utf-8")
-    return f.readlines()
-
-
-STAGES = {0: 'stage_0.png', 1: 'stage_1.png',
-          2: 'stage_2.png', 3: 'stage_3.png',
-          4: 'stage_4.png', 5: 'stage_5.png',
-          6: 'stage_6.png', 7: 'stage_7.png',
-          8: 'stage_8.png', 9: 'stage_9.png',
-          10: 'stage_10.png', }
-
-help(pygame.draw.rect)
 def load_image(name, colorkey=None):
     fname = os.path.join("data", name)
     if not os.path.isfile(fname):
@@ -46,9 +36,9 @@ def load_image(name, colorkey=None):
         img = img.convert()
         if colorkey == -1:
             colorkey = img.get_at((0, 0))
-        img.set_colorkey(colorkey)
-    else:
-        img = img.convert_alpha()
+            img.set_colorkey(colorkey)
+        else:
+            img = img.convert_alpha()
     return img
 
 def draw_cell(letter, letter_col, cell_col, row, col, cell_size=40, not_filled=False):
@@ -62,7 +52,69 @@ def draw_cell(letter, letter_col, cell_col, row, col, cell_size=40, not_filled=F
     text = font.render(f'{letter}', True, letter_col)
     screen.blit(text, (10 + (col + 1) * MARGIN + col * cell_size, 10 + (row + 1) * MARGIN + row * cell_size))
 
+def draw_keyboard():
+    alpha = "qwertyuiopasdfghjklzxcvbnm "
+    row = 6
+    col = 0
+    for letter in alpha:
+        x = row * 40 + (row + 1) * MARGIN
+        y = col * 40 + (col + 1) * MARGIN
+        pygame.draw.rect(screen, (100, 100, 100), (y, x, WIDTH, HEIGHT), border_radius=7)
+        font = pygame.font.Font(None, 40)
+        text = font.render(f'{letter}', True, (255, 255, 255))
+        screen.blit(text, (10 + (col + 1) * MARGIN + col * cell_size, 10 + (row + 1) * MARGIN + row * 40))
+        col += 1
+        if col > 8:
+            row += 1
+            col = 0
 
+class Keyboard:
+    # создание поля
+    def __init__(self, width, height, left=10, top=10, cell_size=40):
+        alpha = "ёйцукенгшщзхъфывапролджэячсмитьбю".upper()
+        self.width = width
+        self.height = height
+        self.board = [list(alpha[:11]), list(alpha[11:22]), list(alpha[22:])]
+        # значения по умолчанию
+        self.left = 0
+        self.top = 0
+        self.cell_size = 0
+        self.set_view(left, top, cell_size)
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, scr):
+        row = 6
+        col = 0
+        for row in range(3):
+            for col in range(11):
+                x = (row + 7) * self.cell_size + (row + 8) * MARGIN
+                y = col * self.cell_size + (col + 1) * MARGIN
+                pygame.draw.rect(screen, (100, 100, 100), (y, x, WIDTH, HEIGHT), border_radius=7)
+                font = pygame.font.Font(None, 40)
+                text = font.render(f'{self.board[row][col]}', True, (255, 255, 255))
+                screen.blit(text, (self.left + (col + 1) * MARGIN + col * self.cell_size, self.top + (row + 8) * MARGIN + (row + 7) * self.cell_size))
+
+    def get_cell(self, mouse_pos):
+        y, x = mouse_pos
+        x = (x - self.cell_size - MARGIN) // (self.cell_size + MARGIN) - 6
+        y = (y - 7 * self.cell_size - 8 * MARGIN) // (self.cell_size + MARGIN) + 7
+        if 0 <= x < 3 and 0 <= y < 11:
+            return x, y
+
+    def get_press(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            x, y = cell
+            return self.on_press(x, y)
+
+    def on_press(self, x, y):
+        # print(x, y)
+        # print(self.board[x][y])
+        return self.board[x][y]
 
 class Board:
     # создание поля
@@ -93,21 +145,43 @@ class Board:
                 else:
                     draw_cell(self.board[row][col], (255, 255, 255), (100, 100, 100), row, col)
 
+    def render_try(self, scr, tries=0):
+        if tries > 0:
+            fon = pygame.transform.scale(load_image(STAGES[tries], -1), (300, 300))
+            screen.blit(fon, (250, 10))
+
+class Word(Board):
+    def __init__(self):
+        super().__init__(WIDTH, HEIGHT)
+        self.board = ["_"] * 5
+
+    def render(self, scr):
+        for col, letter in enumerate(self.board):
+            draw_cell(letter, (255, 0, 255), (100, 100, 100), 6, col)
+        draw_cell("<-", cell_color, (100, 100, 100), 6, 5)
+        draw_cell("\/", cell_color, (100, 100, 100), 6, 6)
 
     def get_cell(self, mouse_pos):
         y, x = mouse_pos
-        x = (x - self.left) // self.cell_size
-        y = (y - self.top) // self.cell_size
-        if 0 <= y < self.width and 0 <= x < self.height:
+        x = (x - self.cell_size - MARGIN) // (self.cell_size + MARGIN)
+        y = (y - 7 * self.cell_size - 8 * MARGIN) // (self.cell_size + MARGIN)
+        if x == 5 and y == -2 or x == 5 and y == -1:
             return x, y
 
     def get_press(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         if cell:
-            self.on_press(cell)
+            x, y = cell
+            if x * y == -10:
+                return True
+            else:
+                return False
 
-    def on_press(self, cell_coords):
-        print(cell_coords)
+    def add_letter(self, x, y):
+        print("YES")
+
+    def clear_letter(self, x, y):
+        print("NO")
 
 def terminate():
     pygame.quit()
@@ -122,8 +196,8 @@ def start_screen():
                   "Чтобы начать игру кликните мышкой"]
     # pygame.init()
     clock = pygame.time.Clock()
-    #fon = pygame.transform.scale(load_image('fon.jpg'), SIZE)
-    #screen.blit(fon, (0, 0))
+    fon = pygame.transform.scale(load_image('fon.jpg'), SIZE)
+    screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
@@ -145,7 +219,10 @@ def start_screen():
         pygame.display.flip()
         clock.tick(FPS)
 
-def end_screen():
+def win_screen():
+    pass
+
+def game_over_screen():
     intro_text = ["GAME OVER", "",
                   "Вы проиграли",
                   "Чтобы начать новую игру, ",
@@ -153,8 +230,8 @@ def end_screen():
                   ]
     # pygame.init()
     clock = pygame.time.Clock()
-    #fon = pygame.transform.scale(load_image('fon.jpg'), SIZE)
-    #screen.blit(fon, (0, 0))
+    fon = pygame.transform.scale(load_image('fon.jpg'), SIZE)
+    screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
@@ -172,35 +249,64 @@ def end_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-
+                start_screen()
                 return
         pygame.display.flip()
         clock.tick(FPS)
 
 start_screen()
-NOUNS = load_from_file("Text/words.txt")
-NORMAL_WORDS = load_from_file("Text/words2.txt")
-
 
 running = True
+
 board = Board(WIDTH, HEIGHT)
+keyboard = Keyboard(WIDTH, HEIGHT)
+word_input = Word()
 tries = 0
-answer = random.choice(NOUNS)
-while running and tries < 6:
+answer = "СЛОВО" # random.choice(NORMAL_WORDS)
+while running and tries <= 6:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or tries == 6:
+        if event.type == pygame.QUIT:
             running = False
-            end_screen()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            terminate()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
             word = list(input().upper())
             while len(word) != 5:
                 print("Введите существительное из 5 букв")
                 word = list(input().upper())
             board.board[tries] = word
+            if word == answer:
+                running = False
             tries += 1
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            board.get_press(event.pos)
+            if keyboard.get_cell(event.pos):
+                last = "".join(word_input.board).strip("_")
+                text = (last + keyboard.get_press(event.pos)).ljust(5, "_")
+                if len(text) == 5:
+                    word_input.board = list(text)
+            if word_input.get_press(event.pos):
+                last = "".join(word_input.board).strip("_")
+                if len(last) > 0:
+                    last = last[:-1]
+                text = last.ljust(5, "_")
+                if len(text) == 5:
+                    word_input.board = list(text)
+            if word_input.get_cell(event.pos):
+                if not word_input.get_press(event.pos):
+                    last = "".join(word_input.board).strip("_")
+                    text = list(last)
+                    if len(text) == 5:
+                        board.board[tries] = list(text)
+                        tries += 1
+                        word_input.board = ["_"] * 5
 
+
+
+    if tries >= 6 or not running:
+        game_over_screen()
+    keyboard.render(screen)
     board.render(screen)
+    word_input.render(screen)
+    if running:
+        board.render_try(screen, tries)
     pygame.display.update()
-    screen.fill((0, 0, 0))
+    screen.fill("#99D9EA")
